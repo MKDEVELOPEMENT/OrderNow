22e195e11540d44593e5b16036abe2decffd8a61
MKDEVELOPEMENT <muaaz2603@gmail.com>
2017-07-20 02:08:24 +0200

    Does it work?????

--- OrderNow/app/src/main/java/com/example/android/ordernow/PlaceOrder.java
+++ OrderNow/app/src/main/java/com/example/android/ordernow/PlaceOrder.java
@@ -77,7 +77,7 @@ public class PlaceOrder extends AppCompatActivity implements LoaderManager.Loade
         return super.onOptionsItemSelected(item);
     }
 
-    int deletemeeee45;
+    int testingComit;
 
     public void sendDataToCurrentOrders(){
         View view;
